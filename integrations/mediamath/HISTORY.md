9f1d26899105db7abdd949d46a2141b269cefa07
SegmentDestinationsBot <tools+github-destinations-bot@segment.com>
2018-07-12 13:52:12 -0700

    Add integration mediamath
    This commit copies the content of the integration repo into
    the "integrations" folder.
    
    Original repo: https://github.com/segment-integrations/analytics.js-integration-mediamath
    Readme: https://github.com/segment-integrations/analytics.js-integration-mediamath/blob/master/README.md

---
+++ integrations/mediamath/HISTORY.md
@@ -0,0 +1,51 @@
+2.0.2 / 2018-02-23
+==================
+
+  * Prep for mixed component migration
+
+2.0.1 / 2016-10-26
+==================
+
+  * Properly support array-based s/v param mappings
+
+2.0.0 / 2016-06-21
+==================
+
+  * Remove Duo compatibility
+  * Add CI setup (coverage, linting, cross-browser compatibility, etc.)
+  * Update eslint configuration
+
+1.1.4 / 2016-05-07
+==================
+
+  * Bump Analytics.js core, tester, integration to use Facade 2.x
+
+1.1.3 / 2015-06-30
+==================
+
+  * Replace analytics.js dependency with analytics.js-core
+
+1.1.2 / 2015-06-24
+==================
+
+  * Bump analytics.js-integration version
+
+1.1.1 / 2015-06-24
+==================
+
+  * Bump analytics.js-integration version
+
+1.1.0 / 2015-06-10
+==================
+
+  * Use MediaMath's JavaScript container rather than their pixel
+
+1.0.1 / 2015-06-10
+==================
+
+  * Bump dependency versions
+
+1.0.0 / 2015-06-09
+==================
+
+  * Initial commit :sparkles:
---
+++ integrations/mediamath/lib/index.js
@@ -0,0 +1,108 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+
+var integration = require('@segment/analytics.js-integration');
+var qs = require('component-querystring');
+var foldl = require('@ndhoule/foldl');
+var each = require('component-each');
+var toNoCase = require('to-no-case');
+
+/**
+ * Expose `MediaMath`
+ */
+
+var MediaMath = module.exports = integration('MediaMath')
+  .option('allPagesMtId', '')
+  .option('allPagesMtAdId', '')
+  .option('events', [])
+  .tag('page', '<script src="//pixel.mathtag.com/event/js?mt_id={{ allPagesMtId }}&mt_adid={{ allPagesMtAdId }}&v1=&v2=&v3=&s1=&s2=&s3=">')
+  .tag('conversion', '<script src="//pixel.mathtag.com/event/js?{{ query }}">');
+
+/**
+ * Initialize.
+ *
+ * @api public
+ */
+
+MediaMath.prototype.initialize = function() {
+  var allPagesMtId = this.options.allPagesMtId;
+  var allPagesMtAdId = this.options.allPagesMtAdId;
+  if (allPagesMtId && allPagesMtAdId) this.load('page');
+  this.ready();
+};
+
+/**
+ * Loaded.
+ *
+ * @api public
+ * @return {boolean}
+ */
+
+MediaMath.prototype.loaded = function() {
+  // MediaMath loads pixels on demand, so it doesn't need to be pre-loaded
+  return true;
+};
+
+/**
+ * Track.
+ *
+ * @api public
+ * @param {Track} track
+ */
+
+MediaMath.prototype.track = function(track) {
+  var events = [];
+  if (!this.options.events || !this.options.events.length) return;
+
+  // retrieve event mappings that match the current event
+  for (var i=0; i<this.options.events.length; i++) {
+    var item = this.options.events[i];
+    if (item.value) {
+      if (toNoCase(item.key) === toNoCase(track.event())) events.push(item.value);
+    } else if (toNoCase(item.event) === toNoCase(track.event())) {
+      events.push(item);
+    }
+  }
+  var self = this;
+  each(events, function(event) {
+    // sParams and vParams are arbitrarily separated,
+    // any of either can be assigned any dynamic value.
+    var queryParamPropertyMap = [].concat(event.vParameters || [], event.sParameters || []);
+
+    // front-load query accumulator with empty string defaults
+    // so that resulting qs will be formatted correctly
+    // without omission of mandatory keys (&v1=&v2=&v3=&s1=&s2=&s3=)
+    var query = foldl(function(query, kv) {
+      var property = kv && kv.key;
+      var mappedKey = kv && kv.value;
+      if (!(property && mappedKey)) return query;
+
+      var value;
+      if (property === 'revenue') {
+        value = (track.revenue() || 0).toFixed(2);
+      } else {
+        value = track.proxy('properties.' + property);
+      }
+
+      if (value !== undefined && value !== null) {
+        query[mappedKey] = value;
+      }
+
+      return query;
+    }, {
+      mt_id: event.mtId || '',
+      mt_adid: event.mtAdId || '',
+      v1: '',
+      v2: '',
+      v3: '',
+      s1: '',
+      s2: '',
+      s3: ''
+    }, queryParamPropertyMap);
+
+    self.load('conversion', { query: qs.stringify(query) });
+  });
+};
