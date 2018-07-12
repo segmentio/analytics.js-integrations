83e902ece2e1e1730171dad3efbd0dfdc50f99f8
SegmentDestinationsBot <tools+github-destinations-bot@segment.com>
2018-07-12 13:58:12 -0700

    Add integration trackjs
    This commit copies the content of the integration repo into
    the "integrations" folder.
    
    Original repo: https://github.com/segment-integrations/analytics.js-integration-trackjs
    Readme: https://github.com/segment-integrations/analytics.js-integration-trackjs/blob/master/README.md

---
+++ integrations/trackjs/README.md
@@ -0,0 +1,12 @@
+# analytics.js-integration-trackjs [![Build Status][ci-badge]][ci-link]
+
+Trackjs integration for [Analytics.js][].
+
+## License
+
+Released under the [MIT license](LICENSE).
+
+
+[Analytics.js]: https://segment.com/docs/libraries/analytics.js/
+[ci-link]: https://ci.segment.com/gh/segment-integrations/analytics.js-integration-trackjs
+[ci-badge]: https://ci.segment.com/gh/segment-integrations/analytics.js-integration-trackjs.svg?style=svg&circle-token=fe7df92f5d1c3457552d799e67fe1ee172dbf1ab
---
+++ integrations/trackjs/lib/index.js
@@ -0,0 +1,91 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+
+var integration = require('@segment/analytics.js-integration');
+var is = require('is');
+var defaults = require('@ndhoule/defaults');
+
+/**
+ * Expose `TrackJS`.
+ */
+
+var TrackJS = module.exports = integration('Track JS')
+  .global('track')
+  .global('trackJs')
+  .global('_trackJs')
+  .option('enabled', true)
+  .option('token', '')
+  .option('application', '')
+  .option('callbackEnabled', true)
+  .option('callbackBindStack', false)
+  .option('consoleEnabled', true)
+  .option('consoleDisplay', true)
+  .option('consoleError', true)
+  .option('networkEnabled', true)
+  .option('networkError', true)
+  .option('visitorEnabled', true)
+  .option('windowEnabled', true)
+  .tag('<script src="//d2zah9y47r7bi2.cloudfront.net/releases/current/tracker.js">');
+
+/**
+ * Initialize.
+ *
+ * @api private
+ */
+
+TrackJS.prototype.initialize = function() {
+  var opts = setOpts(this.options);
+  var user = this.analytics.user();
+  opts.userId = user.id() || '';
+  window._trackJs = window._trackJs || {};
+  window._trackJs = defaults(window._trackJs, opts);
+  this.load(this.ready);
+};
+
+/**
+ * Loaded?
+ *
+ * @return {Boolean}
+ * @api private
+ */
+
+TrackJS.prototype.loaded = function() {
+  return is.object(window.trackJs);
+};
+
+/**
+ * Reconstruct trackjs configuration object from options
+ *
+ * @opts Object
+ */
+
+function setOpts(opts) {
+  return {
+    enabled: opts.enabled,
+    token: opts.token,
+    application: opts.application,
+    callback: {
+      enabled: opts.callbackEnabled,
+      bindStack: opts.callbackBindStack
+    },
+    console: {
+      enabled: opts.consoleEnabled,
+      display: opts.consoleDisplay,
+      error: opts.consoleError,
+      watch: ['log', 'debug', 'info', 'warn', 'error']
+    },
+    network: {
+      enabled: opts.networkEnabled,
+      error: opts.networkError
+    },
+    visitor: {
+      enabled: opts.visitorEnabled
+    },
+    window: {
+      enabled: opts.windowEnabled
+    }
+  };
+}
