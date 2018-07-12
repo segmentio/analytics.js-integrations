a13f3037e0ee4e206d13c9d7077e68582c41f387
SegmentDestinationsBot <tools+github-destinations-bot@segment.com>
2018-07-12 12:48:03 -0700

    Add integration hotjar
    This commit copies the content of the integration repo into
    the "integrations" folder.
    
    Original repo: https://github.com/segment-integrations/analytics.js-integration-hotjar
    Readme: https://github.com/segment-integrations/analytics.js-integration-hotjar/blob/master/README.md

---
+++ integrations/hotjar/test/index.contract.test.js
@@ -0,0 +1,42 @@
+/**
+ * These are the contract tests for the integration. They SHOULD check responses against
+ * recorded network traffic in `contract` mode, and live network responses in `smoke` mode.
+ *
+ * Local development should hit third-party endpoints in `smoke` mode.
+ * CircleCI should hit recorded traffic in `contract` mode.
+ */
+
+'use strict'
+
+var Analytics = require('@segment/analytics.js-core').constructor
+var Sandbox = require('@segment/clear-env')
+var Tester = require('@segment/analytics.js-integration-tester')
+var Hotjar = require('../lib/')
+// var inCircleCI = !!process.env.CIRCLE_TOKEN // If we can access this token var, then we're in CircleCI and should run recorded traffic.
+
+describe('Hotjar Contract', function () {
+  var analytics
+  var hotjar
+  var options = {
+    hjid: 485778,
+    hjTriggers: {},
+    hjTagRecordingEvents: [],
+    hjPlaceholderPolyfill: true
+  }
+
+  beforeEach(function () {
+    analytics = new Analytics()
+    hotjar = new Hotjar(options)
+    analytics.use(Hotjar)
+    analytics.use(Tester)
+    analytics.add(hotjar)
+  })
+
+  afterEach(function () {
+    analytics.restore()
+    analytics.reset()
+    hotjar.reset()
+    Sandbox()
+  })
+  // This is being held for future work on contract/smoke tests.
+})
---
+++ integrations/hotjar/test/index.unit.test.js
@@ -0,0 +1,87 @@
+/*
+ * These are the unit tests for the integration. They should NOT test the network nor any
+ * remote third-party functionality - only that the local code acts and runs as expected.
+ */
+
+'use strict'
+
+var Analytics = require('@segment/analytics.js-core').constructor
+var Integration = require('@segment/analytics.js-integration')
+var Sandbox = require('@segment/clear-env')
+var Tester = require('@segment/analytics.js-integration-tester')
+var Hotjar = require('../lib/')
+
+describe('Hotjar Unit', function () {
+  var analytics
+  var hotjar
+  var customOptions
+  var options = {
+    hjid: 485778,
+    hjPlaceholderPolyfill: true
+  }
+
+  beforeEach(function () {
+    analytics = new Analytics()
+    hotjar = new Hotjar(options)
+    analytics.use(Hotjar)
+    analytics.use(Tester)
+    analytics.add(hotjar)
+
+    customOptions = {
+      hjid: 485778,
+      hjPlaceholderPolyfill: false
+    }
+  })
+
+  afterEach(function () {
+    analytics.restore()
+    analytics.reset()
+    hotjar.reset()
+    Sandbox()
+  })
+
+  describe('Constructing the integration', function () {
+    it('should have the right settings', function () {
+      analytics.compare(Hotjar, Integration('Hotjar')
+      .option('hjid', null)
+      .option('hjPlaceholderPolyfill', true))
+    })
+
+    it('should have the right settings with custom options', function () {
+      hotjar = new Hotjar(customOptions) // Intentionally creating a new Hotjar integration here to not simply compare the object to itself.
+
+      analytics.deepEqual(hotjar.options, customOptions)
+    })
+  })
+
+  describe('#initialize', function () {
+    it('should call this.ready() with valid options', function () {
+      analytics.stub(hotjar, 'ready')
+      hotjar.initialize()
+      analytics.called(hotjar.ready)
+    })
+
+    it('should prepare the needed globals', function () {
+      hotjar.initialize()
+      analytics.deepEqual(window._hjSettings, {hjid: options.hjid, hjsv: 5, hjPlaceholderPolyfill: true})
+      analytics.assert(typeof window.hj === 'function')
+    })
+
+    it('should reject an invalid HJID', function () {
+      customOptions.hjid = NaN
+      testInvalidInitialize(customOptions)
+    })
+
+    it('should reject an invalid hjPlaceholderPolyfill boolean', function () {
+      customOptions.hjPlaceholderPolyfill = 1
+      testInvalidInitialize(customOptions)
+    })
+
+    function testInvalidInitialize (invalidOptions) {
+      hotjar.options = invalidOptions
+      analytics.stub(hotjar, 'ready')
+      hotjar.initialize()
+      analytics.didNotCall(hotjar.ready)
+    }
+  })
+})
