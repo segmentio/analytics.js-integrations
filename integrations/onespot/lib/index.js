fcffd642a24e556f3447c7e3d19d240647525aea
SegmentDestinationsBot <tools+github-destinations-bot@segment.com>
2018-07-12 13:57:12 -0700

    Add integration onespot
    This commit copies the content of the integration repo into
    the "integrations" folder.
    
    Original repo: https://github.com/segment-integrations/analytics.js-integration-onespot
    Readme: https://github.com/segment-integrations/analytics.js-integration-onespot/blob/master/README.md

---
+++ integrations/onespot/lib/index.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+
+var integration = require('@segment/analytics.js-integration');
+
+/**
+ * Expose `RocketFuel`.
+ */
+
+var OneSpot = module.exports = integration('OneSpot')
+  .option('accountId', '')
+  .tag('<script src="//d3xl0zyjyljwa.cloudfront.net/px/t/os-{{ accountId }}-0.js"></script>');
+
+/**
+ * Loaded.
+ *
+ * @return {Boolean}
+ */
+
+OneSpot.prototype.loaded = function() {
+  return !!window.osANSegCode;
+};
+
+/**
+ * Initialize.
+ *
+ * @api public
+ */
+
+OneSpot.prototype.initialize = function() {
+  this.load({ accountId: this.options.accountId }, this.ready);
+};
