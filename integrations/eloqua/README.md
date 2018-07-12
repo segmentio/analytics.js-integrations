0bea5df0b03ad7dec2b697c54f2d8323764f0826
SegmentDestinationsBot <tools+github-destinations-bot@segment.com>
2018-07-12 12:44:52 -0700

    Add integration eloqua
    This commit copies the content of the integration repo into
    the "integrations" folder.
    
    Original repo: https://github.com/segment-integrations/analytics.js-integration-eloqua
    Readme: https://github.com/segment-integrations/analytics.js-integration-eloqua/blob/master/README.md

---
+++ integrations/eloqua/README.md
@@ -0,0 +1,12 @@
+# analytics.js-integration-eloqua [![Build Status][ci-badge]][ci-link]
+
+Eloqua integration for [Analytics.js][].
+
+## License
+
+Released under the [MIT license](LICENSE).
+
+
+[Analytics.js]: https://segment.com/docs/libraries/analytics.js/
+[ci-link]: https://ci.segment.com/gh/segment-integrations/analytics.js-integration-eloqua
+[ci-badge]: https://ci.segment.com/gh/segment-integrations/analytics.js-integration-eloqua.svg?style=svg&circle-token=44ff8cc0fa74c709a39adc1b35c8bd9bbe4db687
---
+++ integrations/eloqua/lib/index.js
@@ -0,0 +1,53 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+
+var push = require('global-queue')('_elqQ');
+var integration = require('@segment/analytics.js-integration');
+
+/**
+ * Expose `Eloqua`
+ */
+
+var Eloqua = module.exports = integration('Eloqua')
+  .assumesPageview()
+  .global('_elq')
+  .global('_elqQ')
+  .option('siteId', '')
+  .tag('<script src="//img.en25.com/i/elqCfg.min.js">');
+
+/**
+ * Initialize.
+ *
+ * @api public
+ */
+
+Eloqua.prototype.initialize = function() {
+  push('elqSetSiteId', this.options.siteId);
+  this.load(this.ready);
+};
+
+/**
+ * Loaded?
+ *
+ * @api public
+ * @return {boolean}
+ */
+
+Eloqua.prototype.loaded = function() {
+  return !!window._elqQ;
+};
+
+/**
+ * Page
+ *
+ * @api public
+ * @param {Page}
+ */
+
+Eloqua.prototype.page = function(page) {
+  var props = page.properties();
+  push('elqTrackPageView', props.url, props.referrer);
+};
