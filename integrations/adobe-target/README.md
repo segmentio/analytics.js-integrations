8348df6b4522887fca5adad846624a13c8d7c9ec
SegmentDestinationsBot <tools+github-destinations-bot@segment.com>
2018-07-12 15:06:33 -0700

    Add integration adobe-target
    This commit copies the content of the integration repo into
    the "integrations" folder.
    
    Original repo: https://github.com/segment-integrations/analytics.js-integration-adobe-target
    Readme: https://github.com/segment-integrations/analytics.js-integration-adobe-target/blob/master/README.md

---
+++ integrations/adobe-target/README.md
@@ -0,0 +1,12 @@
+# analytics.js-integration-adobe-target [![Build Status][ci-badge]][ci-link]
+
+Adobe Target integration for [Analytics.js][].
+
+## License
+
+Released under the [MIT license](LICENSE).
+
+
+[Analytics.js]: https://segment.com/docs/libraries/analytics.js/
+[ci-link]: https://ci.segment.com/gh/segment-integrations/analytics.js-integration-adobe-target
+[ci-badge]: https://ci.segment.com/gh/segment-integrations/analytics.js-integration-adobe-target.svg?style=svg
---
+++ integrations/adobe-target/lib/index.js
@@ -0,0 +1,74 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+
+var integration = require('@segment/analytics.js-integration');
+
+/**
+ * Expose `Adobe Target` integration.
+ */
+
+var AdobeTarget = module.exports = integration('Adobe Target');
+
+AdobeTarget.prototype.initialize = function() {
+  // make sure the integration doesn't break in case at.js isn't included manually
+  window.adobe = window.adobe || {};
+  window.adobe.target = window.adobe.target || {};
+  window.adobe.target.trackEvent = window.adobe.target.trackEvent || function() { /* noop */ };
+  this.ready();
+};
+
+AdobeTarget.prototype.track = function(track) {
+  var mbox = getMboxName(track);
+
+  if (mbox === undefined) {
+    return;
+  }
+
+  window.adobe.target.trackEvent({
+    mbox: mbox,
+    params: track.properties()
+  });
+};
+
+AdobeTarget.prototype.orderCompleted = function(track) {
+  var mbox = getMboxName(track);
+
+  if (mbox === undefined) {
+    return;
+  }
+
+  var products = track.products() || [];
+  var productIdArray = [];
+
+  for (var i = 0; i < products.length; i++) {
+    var productId = products[i].id || products[i].productId || products[i].product_id;
+    if (productId) productIdArray.push(productId);
+  }
+
+  window.adobe.target.trackEvent({
+    mbox: mbox,
+    params: {
+      orderId: track.orderId(), 
+      orderTotal: track.revenue(),
+      productPurchaseId: productIdArray.join(',')
+    }
+  });
+};
+
+function getMboxName(msg) {
+  var opts = msg.options('Adobe Target');
+
+  if (!opts.mboxName) {
+    return;
+  }
+
+  var mbox = opts.mboxName;
+
+  if (typeof mbox !== 'string') {
+    mbox = mbox.toString();
+  }
+  return mbox;
+}
