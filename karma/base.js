b532a3eb186de9dd871eeb291393fd72b42f156e
Carlos Mecha <mecha.carlos@gmail.com>
2018-08-09 16:10:19 -0700

    Improve SauceLabs configuration (#41)
    * Add global karma, fix configuration
    
    * Try with 1 tunnel

---
+++ karma/base.js
@@ -0,0 +1,37 @@
+
+/**
+ * Base configuration for Karma.
+ *
+ * Do not touch unless you know what you are doing.
+ */
+module.exports = {
+  exclude: [
+    'integrations/**/node_modules/**'
+  ],
+  frameworks: ['browserify', 'mocha'],
+  reporters: ['spec'],
+  browserDisconnectTimeout: 60000,
+  browserNoActivityTimeout: 60000,
+  concurrency: 1,
+  singleRun: true,
+  client: {
+    captureConsole: false,
+    mocha: {
+      reporter: 'html',
+      timeout: 10000
+    }
+  },
+  browserify: {
+    debug: true,
+    transform: [
+      [
+        'browserify-istanbul',
+        {
+          instrumenterConfig: {
+            embedSource: true
+          }
+        }
+      ]
+    ]
+  }
+}
---
+++ karma/firefox.js
@@ -0,0 +1,21 @@
+
+function setFirefox (config) {
+  config.customLaunchers = {
+    sl_firefox_latest: {
+      base: 'SauceLabs',
+      browserName: 'firefox',
+      platform: 'linux',
+      version: 'latest'
+    },
+    sl_firefox_latest_1: {
+      base: 'SauceLabs',
+      browserName: 'firefox',
+      platform: 'linux',
+      version: 'latest-1'
+    }
+  }
+  config.browsers = Object.keys(config.customLaunchers)
+  return config
+}
+
+module.exports = setFirefox
