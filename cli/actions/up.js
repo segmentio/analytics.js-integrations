4f137b31f7ff82e3d97ca4d862efa22d9ac5cbb5
Flaque <econrad@zagmail.gonzaga.edu>
2018-08-14 16:13:42 -0700

    Add template renderering

--- cli/actions/up.js
+++ cli/actions/up.js
@@ -73,16 +73,20 @@ async function readTemplates() {
 
   return {
     minifiedTemplate: await fs.readFile(
-      path.join(AJS_PRIVATE_LOCATION, "templates", "analytics.min.js")
+      path.join(AJS_PRIVATE_LOCATION, "templates", "analytics.min.js"),
+      "utf8"
     ),
     unminifiedTemplate: await fs.readFile(
-      path.join(AJS_PRIVATE_LOCATION, "templates", "analytics.js")
+      path.join(AJS_PRIVATE_LOCATION, "templates", "analytics.js"),
+      "utf8"
     ),
     minifiedPlatformTemplate: await fs.readFile(
-      path.join(AJS_PRIVATE_LOCATION, "templates", "platform.min.js")
+      path.join(AJS_PRIVATE_LOCATION, "templates", "platform.min.js"),
+      "utf8"
     ),
     unminifiedPlatformTemplate: await fs.readFile(
-      path.join(AJS_PRIVATE_LOCATION, "templates", "platform.js")
+      path.join(AJS_PRIVATE_LOCATION, "templates", "platform.js"),
+      "utf8"
     )
   };
 }
@@ -93,12 +97,20 @@ async function makeAndReadTemplates() {
 }
 
 function up(yargs) {
+  // Run with --no-rebuild to avoid running make (which can be slow)
+  // This is mostly for testing / dev of the tool
   const { rebuild } = yargs.boolean("rebuild").argv;
   const getTemplates = rebuild ? makeAndReadTemplates : readTemplates;
 
+  // Note that even though Builder.render is "static", "static" in JS does
+  // not work the same way here. Newing up a new instance of Builder
+  // allows us to have a `this` inside Builder.render; without it
+  // we'll crash.
+  const builder = new Builder();
+
   getTemplates()
     .then(templates => buildArgs(templates))
-    .then(Builder.render)
+    .then(builder.render)
     .then(console.log)
     .catch(reportErr);
 }
