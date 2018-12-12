6b1555288ce75b19e5000dd44b7eb89a9b7cbffd
Daniel Archer <me@dja.io>
2018-12-11 16:49:10 -0800

    WIP - Test with existing traits

--- integrations/pinterest-tag/lib/index.js
+++ integrations/pinterest-tag/lib/index.js
@@ -1,179 +1,191 @@
-'use strict'
+"use strict";
 
 /**
  * Module dependencies.
  */
 
-var integration = require('@segment/analytics.js-integration')
-var analyticsEvents = require('analytics-events')
+var integration = require("@segment/analytics.js-integration");
+var analyticsEvents = require("analytics-events");
 
 /**
  * Expose `Pinterest` integration.
  */
 
-var Pinterest = module.exports = integration('Pinterest Tag')
-  .global('pintrk')
-  .option('tid', '')
-  .option('initWithExistingTraits', false)
-  .option('pinterestCustomProperties', [])
-  .mapping('pinterestEventMapping')
-  .tag('<script src="https://s.pinimg.com/ct/core.js"></script>')
+var Pinterest = (module.exports = integration("Pinterest Tag")
+  .global("pintrk")
+  .option("tid", "")
+  .option("initWithExistingTraits", false)
+  .option("pinterestCustomProperties", [])
+  .mapping("pinterestEventMapping")
+  .tag('<script src="https://s.pinimg.com/ct/core.js"></script>'));
 
-Pinterest.prototype.initialize = function () {
+Pinterest.prototype.initialize = function() {
   // We require a Tag ID to run this integration.
-  if (!this.options.tid) return
+  if (!this.options.tid) return;
 
   // Preparation for loading the Pinterest script.
-  (function(e){if(!window.pintrk){window.pintrk=function(){window.pintrk.queue.push(Array.prototype.slice.call(arguments))};var n=window.pintrk;n.queue=[],n.version="3.0";}})(); // eslint-disable-line
+  (function(e) {
+    if (!window.pintrk) {
+      window.pintrk = function() {
+        window.pintrk.queue.push(Array.prototype.slice.call(arguments));
+      };
+      var n = window.pintrk;
+      (n.queue = []), (n.version = "3.0");
+    }
+  })(); // eslint-disable-line
 
-  this.load(this.ready)
-  
+  this.load(this.ready);
   if (this.options.initWithExistingTraits) {
-    var traits = formatTraits(this.analytics)
-    pintrk('load', this.options.tid, traits)
+    var traits = formatTraits(this.analytics);
+    pintrk("load", this.options.tid, traits);
   } else {
-    pintrk('load', this.options.tid)
+    pintrk("load", this.options.tid);
   }
-  pintrk('page') // This is treated semantically different than our own page implementation.
+  pintrk("page"); // This is treated semantically different than our own page implementation.
 
-  this.createPropertyMapping()
-}
+  this.createPropertyMapping();
+};
 
-Pinterest.prototype.loaded = function () {
-  return !!(window.pintrk && window.pintrk.queue && window.pintrk.queue.push !== Array.prototype.push)
-}
+Pinterest.prototype.loaded = function() {
+  return !!(
+    window.pintrk &&
+    window.pintrk.queue &&
+    window.pintrk.queue.push !== Array.prototype.push
+  );
+};
 
-Pinterest.prototype.page = function (page) {
+Pinterest.prototype.page = function(page) {
   // If we have a category, the use ViewCategory. Otherwise, use a normal PageVisit.
   if (page.category()) {
-    pintrk('track', 'ViewCategory', {
+    pintrk("track", "ViewCategory", {
       category: page.category(),
-      name: page.name() || ''
-    })
+      name: page.name() || ""
+    });
   } else {
-    pintrk('track', 'PageVisit', {
-      name: page.name() || ''
-    })
+    pintrk("track", "PageVisit", {
+      name: page.name() || ""
+    });
   }
-}
+};
 
-Pinterest.prototype.track = function (track) {
+Pinterest.prototype.track = function(track) {
   // Send a Pinterest Event if mapped. Otherwise, send the call as-is.
-  var segmentEvent = track.event()
-  var pinterestEvent = this.getPinterestEvent(segmentEvent)
-  var pinterestObject = this.generatePropertiesObject(track)
+  var segmentEvent = track.event();
+  var pinterestEvent = this.getPinterestEvent(segmentEvent);
+  var pinterestObject = this.generatePropertiesObject(track);
 
-  pinterestEvent ? pintrk('track', pinterestEvent, pinterestObject) : pintrk('track', segmentEvent, pinterestObject)
-}
+  pinterestEvent
+    ? pintrk("track", pinterestEvent, pinterestObject)
+    : pintrk("track", segmentEvent, pinterestObject);
+};
 
-Pinterest.prototype.getPinterestEvent = function (segmentEvent) {
+Pinterest.prototype.getPinterestEvent = function(segmentEvent) {
   for (var mappedEvent in this.options.pinterestEventMapping) {
     if (mappedEvent.toLowerCase() === segmentEvent.toLowerCase()) {
-      return this.options.pinterestEventMapping[mappedEvent]
+      return this.options.pinterestEventMapping[mappedEvent];
     }
   }
 
   var eventMap = [
     // Segment Inbound Event (Regex) -> Pinterest Outbound Event
-    [analyticsEvents.productsSearched, 'Search'],
-    [analyticsEvents.productListFiltered, 'Search'],
-    [analyticsEvents.productAdded, 'AddToCart'],
-    [analyticsEvents.orderCompleted, 'Checkout'],
-    [analyticsEvents.videoPlaybackStarted, 'WatchVideo']
-  ]
+    [analyticsEvents.productsSearched, "Search"],
+    [analyticsEvents.productListFiltered, "Search"],
+    [analyticsEvents.productAdded, "AddToCart"],
+    [analyticsEvents.orderCompleted, "Checkout"],
+    [analyticsEvents.videoPlaybackStarted, "WatchVideo"]
+  ];
 
   for (var index in eventMap) {
-    var eventRegex = eventMap[index][0]
-    var pinterestEvent = eventMap[index][1]
+    var eventRegex = eventMap[index][0];
+    var pinterestEvent = eventMap[index][1];
 
     if (eventRegex.test(segmentEvent)) {
-      return pinterestEvent
+      return pinterestEvent;
     }
   }
-}
+};
 
 /**
  * Generate the property mappings for the integration. Account for product information being nested in a `products` array.
  */
 
-Pinterest.prototype.createPropertyMapping = function () {
+Pinterest.prototype.createPropertyMapping = function() {
   this.propertyMap = {
     // Segment Property: Pinterest Property
-    'query': 'search_query',
-    'order_id': 'order_id',
-    'coupon': 'coupon',
-    'value': 'value',
-    'currency': 'currency'
-  }
+    query: "search_query",
+    order_id: "order_id",
+    coupon: "coupon",
+    value: "value",
+    currency: "currency"
+  };
 
   // This is a second map to allow us to loop over specific potentially-nested properties.
   this.productPropertyMap = {
     // Segment Property: Pinterest Property
-    'name': 'product_name',
-    'product_id': 'product_id',
-    'sku': 'product_id',
-    'category': 'product_category',
-    'variant': 'product_variant',
-    'price': 'product_price',
-    'quantity': 'product_quantity',
-    'brand': 'product_brand'
-  }
-}
+    name: "product_name",
+    product_id: "product_id",
+    sku: "product_id",
+    category: "product_category",
+    variant: "product_variant",
+    price: "product_price",
+    quantity: "product_quantity",
+    brand: "product_brand"
+  };
+};
 
 /**
  * Fill our Properties for the pintrk() call.
  */
 
-Pinterest.prototype.generatePropertiesObject = function (track) {
+Pinterest.prototype.generatePropertiesObject = function(track) {
   // Generate the properties object to send with the call.
-  var pinterestProps = {}
-  var trackValue
+  var pinterestProps = {};
+  var trackValue;
   for (var prop in this.propertyMap) {
-    trackValue = track.proxy('properties.' + prop)
-    if (trackValue) pinterestProps[this.propertyMap[prop]] = trackValue
+    trackValue = track.proxy("properties." + prop);
+    if (trackValue) pinterestProps[this.propertyMap[prop]] = trackValue;
   }
 
   // Determine if there's a 'products' Array, then add in the specific features on that decision.
-  var products = track.proxy('properties.products')
-  var lineItemsArray
+  var products = track.proxy("properties.products");
+  var lineItemsArray;
   if (Array.isArray(products)) {
-    lineItemsArray = []
+    lineItemsArray = [];
     for (var i = 0; i < products.length; i++) {
       for (prop in this.productPropertyMap) {
-        trackValue = products[i][prop]
+        trackValue = products[i][prop];
         if (trackValue) {
           // Product values are added into a `line_items` array, with a nested object. If that doesn't exist, make it first.
-          if (lineItemsArray[i] === undefined) lineItemsArray[i] = {}
-          lineItemsArray[i][this.productPropertyMap[prop]] = trackValue
+          if (lineItemsArray[i] === undefined) lineItemsArray[i] = {};
+          lineItemsArray[i][this.productPropertyMap[prop]] = trackValue;
         }
       }
     }
-    if (lineItemsArray.length) pinterestProps['line_items'] = lineItemsArray
+    if (lineItemsArray.length) pinterestProps["line_items"] = lineItemsArray;
   } else {
     // There will only be a single layer, since we have, at most, one product.
-    lineItemsArray = [{}]
-    var propAdded = false
+    lineItemsArray = [{}];
+    var propAdded = false;
     for (prop in this.productPropertyMap) {
-      trackValue = track.proxy('properties.' + prop)
+      trackValue = track.proxy("properties." + prop);
       if (trackValue) {
-        lineItemsArray[0][this.productPropertyMap[prop]] = trackValue
-        propAdded = true
+        lineItemsArray[0][this.productPropertyMap[prop]] = trackValue;
+        propAdded = true;
       }
     }
-    if (propAdded) pinterestProps['line_items'] = lineItemsArray
+    if (propAdded) pinterestProps["line_items"] = lineItemsArray;
   }
 
   // Finally, add in any custom properties defined by the user.
-  var customProps = this.options.pinterestCustomProperties
+  var customProps = this.options.pinterestCustomProperties;
   for (var j = 0; j < customProps.length; j++) {
-    prop = customProps[j]
-    trackValue = track.proxy('properties.' + prop)
-    if (trackValue) pinterestProps[prop] = trackValue
+    prop = customProps[j];
+    trackValue = track.proxy("properties." + prop);
+    if (trackValue) pinterestProps[prop] = trackValue;
   }
 
-  return pinterestProps
-}
-
+  return pinterestProps;
+};
 
 /**
  * Get Traits Formatted Correctly for Pinterest.
--- integrations/pinterest-tag/test/index.test.js
+++ integrations/pinterest-tag/test/index.test.js
@@ -1,96 +1,98 @@
-'use strict'
+"use strict";
 
-var Analytics = require('@segment/analytics.js-core').constructor
-var integration = require('@segment/analytics.js-integration')
-var tester = require('@segment/analytics.js-integration-tester')
-var sandbox = require('@segment/clear-env')
-var Pinterest = require('../lib/')
+var Analytics = require("@segment/analytics.js-core").constructor;
+var integration = require("@segment/analytics.js-integration");
+var tester = require("@segment/analytics.js-integration-tester");
+var sandbox = require("@segment/clear-env");
+var Pinterest = require("../lib/");
 
-describe('Pinterest', function () {
-  var analytics
-  var pinterest
+describe("Pinterest", function() {
+  var analytics;
+  var pinterest;
   var options = {
-    tid: '2620795819800',
+    tid: "2620795819800",
     pinterestEventMapping: {
-      'Some Custom Event': 'Custom',
-      'Lead Generated': 'Lead',
-      'User Signed Up': 'Signup'
+      "Some Custom Event": "Custom",
+      "Lead Generated": "Lead",
+      "User Signed Up": "Signup"
     },
-    pinterestCustomProperties: [
-      'custom_prop'
-    ],
+    pinterestCustomProperties: ["custom_prop"],
     initWithExistingTraits: false
-  }
+  };
 
-  beforeEach(function () {
-    analytics = new Analytics()
-    pinterest = new Pinterest(options)
-    analytics.use(Pinterest)
-    analytics.use(tester)
-    analytics.add(pinterest)
-    analytics.identify('123', {
-      name: 'Ash Ketchum',
-      email: 'ash@ketchum.com',
-      gender: 'Male',
-      birthday: '01/13/1991',
+  beforeEach(function() {
+    analytics = new Analytics();
+    pinterest = new Pinterest(options);
+    analytics.use(Pinterest);
+    analytics.use(tester);
+    analytics.add(pinterest);
+    analytics.identify("123", {
+      name: "Ash Ketchum",
+      email: "ash@ketchum.com",
+      gender: "Male",
+      birthday: "01/13/1991",
       address: {
-        city: 'Emerald',
-        state: 'Kanto',
+        city: "Emerald",
+        state: "Kanto",
         postalCode: 123456
       }
     });
-  })
-
-  afterEach(function () {
-    analytics.restore()
-    analytics.reset()
-    pinterest.reset()
-    sandbox()
-  })
-
-  it('should have the right settings', function () {
-    analytics.compare(Pinterest, integration('Pinterest Tag')
-      .global('pintrk')
-      .mapping('pinterestEventMapping')
-      .option('pinterestCustomProperties', [])
-      .option('tid', '')
-      .option('initWithExistingTraits', false))
-  })
-
-  before(function() {
-    options.initWithExistingTraits = true;
   });
 
-  after(function() {
-    options.initWithExistingTraits = false;
+  afterEach(function() {
+    analytics.restore();
+    analytics.reset();
+    pinterest.reset();
+    sandbox();
   });
 
-  it('should call init with the user\'s traits if option enabled', function() {
-    var payload = {
-      em: 'ash@ketchum.com',
-    };
-    analytics.stub(window, 'pintrk');
-    analytics.initialize();
-    analytics.called(window.pintrk, 'init', options.tid, payload);
+  it("should have the right settings", function() {
+    analytics.compare(
+      Pinterest,
+      integration("Pinterest Tag")
+        .global("pintrk")
+        .mapping("pinterestEventMapping")
+        .option("pinterestCustomProperties", [])
+        .option("tid", "")
+        .option("initWithExistingTraits", false)
+    );
   });
 
-  describe('before loading', function () {
-    beforeEach(function () {
-      analytics.stub(pinterest, 'load')
-    })
+  describe("before loading", function() {
+    beforeEach(function() {
+      analytics.stub(pinterest, "load");
+    });
 
-    describe('#initialize', function () {
-      it('should call #load', function () {
-        analytics.initialize()
-        analytics.page()
-        analytics.called(pinterest.load)
-      })
-    })
-  })
+    describe("#initialize", function() {
+      it("should call #load", function() {
+        analytics.initialize();
+        analytics.page();
+        analytics.called(pinterest.load);
+      });
 
-  describe('loading', function () {
-    it('should load', function (done) {
-      analytics.load(pinterest, done)
-    })
-  })
-})
+      describe("with existing traits", function() {
+        before(function() {
+          options.initWithExistingTraits = true;
+        });
+
+        after(function() {
+          options.initWithExistingTraits = false;
+        });
+
+        it("should call init with the user's traits if option enabled", function() {
+          var payload = {
+            em: "ash@ketchum.com"
+          };
+          analytics.initialize();
+          analytics.called(pinterest.load, options.tid, payload);
+        });
+      });
+    });
+  });
+
+  describe("loading", function() {
+    it("should load", function(done) {
+      analytics.load(pinterest, done);
+    });
+  });
+});
