eff51bccd22383f0fdc259d611076738e2677c39
Chris Sperandio <cs@Chriss-MacBook-Pro-3.local>
2018-12-13 16:36:27 -0800

    support quora events

--- integrations/quora-conversion-pixel/lib/index.js
+++ integrations/quora-conversion-pixel/lib/index.js
@@ -1,44 +1,49 @@
-'use strict'
+'use strict';
 
 /**
  * Module dependencies.
  */
 
-var integration = require('@segment/analytics.js-integration')
+var integration = require('@segment/analytics.js-integration');
+var each = require('@ndhoule/each');
 
 /**
  * Expose `Quora Conversion Pixel` integration.
  */
 
-var Quora = module.exports = integration('Quora Conversion Pixel')
+var Quora = (module.exports = integration('Quora Conversion Pixel')
   .global('qp')
   .option('writeKey', '')
   .option('trackEvents', [])
-  .tag('<script src="https://a.quora.com/qevents.js"></script>')
+  .mapping('events')
+  .tag('<script src="https://a.quora.com/qevents.js"></script>'));
 
-Quora.prototype.initialize = function () {
+Quora.prototype.initialize = function() {
   // We require a write key to run this integration.
-  if (!this.options.writeKey) return
+  if (!this.options.writeKey) return;
 
   (function () {if(window.qp) return; var n=window.qp=function(){n.qp?n.qp.apply(n,arguments):n.queue.push(arguments);}; n.queue=[];})() // eslint-disable-line
 
-  this.load(this.ready)
-  window.qp('init', this.options.writeKey)
-  window.qp('track', 'ViewContent')
-}
+  this.load(this.ready);
+  window.qp('init', this.options.writeKey);
+  window.qp('track', 'ViewContent');
+};
 
-Quora.prototype.loaded = function () {
-  return window.qp
-  //return !!(window.qp && window.qp.uuid)
-}
+Quora.prototype.loaded = function() {
+  return window.qp;
+};
 
-Quora.prototype.track = function (track) {
-  // Right now, Quora only supports a single kind of track event. If the user wants a specific event name to fire it, then do so.
-  for (var i = 0; i < this.options.trackEvents.length; i++) {
-    var currentPermittedEvent = this.options.trackEvents[i]
-    if (currentPermittedEvent.toLowerCase() === track.event().toLowerCase()) {
-      window.qp('track', 'Generic')
-      return
+Quora.prototype.track = function(track) {
+  each(function(e) {
+    window.qp('track', e);
+  }, this.events(track.event()));
+
+  // Historically, Quora only supported a single kind of track event (Generic)
+  // We allowed the user to map any number of events to that Generic type
+  // We'll remove this after migrating existing `trackEvents` into `events` setting
+  each(function(e) {
+    if (e.toLowerCase() === track.event().toLowerCase()) {
+      window.qp('track', 'Generic');
     }
-  }
-}
+  }, this.options.trackEvents);
+};
