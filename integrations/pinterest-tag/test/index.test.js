66b712c4fc46509075f0d71eef24c6c43ebb42b2
ccnixon <chris.nixon@segment.com>
2019-03-05 09:57:27 -0800

    Fix tests and run formatter

--- integrations/pinterest-tag/test/index.test.js
+++ integrations/pinterest-tag/test/index.test.js
@@ -1,14 +1,14 @@
-'use strict'
+'use strict';
 
-var Analytics = require('@segment/analytics.js-core').constructor
-var integration = require('@segment/analytics.js-integration')
-var tester = require('@segment/analytics.js-integration-tester')
-var sandbox = require('@segment/clear-env')
-var Pinterest = require('../lib/')
+var Analytics = require('@segment/analytics.js-core').constructor;
+var integration = require('@segment/analytics.js-integration');
+var tester = require('@segment/analytics.js-integration-tester');
+var sandbox = require('@segment/clear-env');
+var Pinterest = require('../lib/');
 
-describe('Pinterest', function () {
-  var analytics
-  var pinterest
+describe('Pinterest', function() {
+  var analytics;
+  var pinterest;
   var options = {
     tid: '2620795819800',
     pinterestEventMapping: {
@@ -16,74 +16,77 @@ describe('Pinterest', function () {
       'Lead Generated': 'Lead',
       'User Signed Up': 'Signup'
     },
-    pinterestCustomProperties: [
-      'custom_prop'
-    ]
-  }
+    pinterestCustomProperties: ['custom_prop']
+  };
 
-  beforeEach(function () {
-    analytics = new Analytics()
-    pinterest = new Pinterest(options)
-    analytics.use(Pinterest)
-    analytics.use(tester)
-    analytics.add(pinterest)
-  })
+  beforeEach(function() {
+    analytics = new Analytics();
+    pinterest = new Pinterest(options);
+    analytics.use(Pinterest);
+    analytics.use(tester);
+    analytics.add(pinterest);
+  });
 
-  afterEach(function () {
-    analytics.restore()
-    analytics.reset()
-    pinterest.reset()
-    sandbox()
-  })
+  afterEach(function() {
+    analytics.restore();
+    analytics.reset();
+    pinterest.reset();
+    sandbox();
+  });
 
-  it('should have the right settings', function () {
-    analytics.compare(Pinterest, integration('Pinterest Tag')
-      .global('pintrk')
-      .mapping('pinterestEventMapping')
-      .option('pinterestCustomProperties', [])
-      .option('tid', ''))
-  })
+  it('should have the right settings', function() {
+    analytics.compare(
+      Pinterest,
+      integration('Pinterest Tag')
+        .global('pintrk')
+        .mapping('pinterestEventMapping')
+        .option('pinterestCustomProperties', [])
+        .option('tid', '')
+    );
+  });
 
-  describe('before loading', function () {
-    beforeEach(function () {
-      analytics.stub(pinterest, 'load')
-    })
+  describe('before loading', function() {
+    beforeEach(function() {
+      analytics.stub(pinterest, 'load');
+    });
 
-    describe('#initialize', function () {
-      it('should call #load', function () {
-        analytics.initialize()
-        analytics.page()
-        analytics.called(pinterest.load, 2620795819800)
-      })
-    })
-  })
+    describe('#initialize', function() {
+      it('should call #load', function() {
+        analytics.initialize();
+        analytics.page();
+        analytics.called(pinterest.load);
+      });
+    });
+  });
 
-  describe('loading', function () {
-    it('should load', function (done) {
-      analytics.load(pinterest, done)
-    })
-  })
+  describe('loading', function() {
+    it('should load', function(done) {
+      analytics.load(pinterest, done);
+    });
+  });
 
-  describe('after loading', function () {
-    beforeEach(function (done) {
-      analytics.once('ready', done)
-      analytics.initialize()
-      analytics.stub(pinterest, 'load')
-    })
+  describe('after loading', function() {
+    beforeEach(function(done) {
+      analytics.once('ready', done);
+      analytics.initialize();
+      analytics.stub(pinterest, 'load');
+    });
 
-    describe('#identify', function () {
-      beforeEach(function () {
-        analytics.stub(window, 'Pinterest')
-      })
+    describe('#identify', function() {
+      beforeEach(function() {
+        analytics.spy(window, 'pintrk');
+      });
 
-      it('should not fire the Pinterest pixel tag', function () {
-        analytics.identify()
-	analytics.didNotCall(pinterest.load)
-      })
-      it('should push Segment email to Pinterest Enhanced Match', function () {
-        analytics.identify('123', { email: 'prakash@segment.com'})
-        analytics.called(pinterest.load, 2620795819800, {em: "prakash@segment.com"})
-      })
-    })
-  })
-})
+      it('should not fire the Pinterest pixel tag', function() {
+        analytics.identify();
+        analytics.didNotCall(window.pintrk);
+      });
+      it('should push Segment email to Pinterest Enhanced Match', function() {
+        analytics.identify('123', { email: 'prakash@segment.com' });
+        analytics.called(window.pintrk, 'load', '2620795819800', {
+          em: 'prakash@segment.com'
+        });
+      });
+    });
+  });
+});
