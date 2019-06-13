60989d228b0a291333533f45f0f69726ef43a856
ccnixon <chris.nixon@segment.com>
2019-06-13 10:59:26 -0700

    Update saucelabs config

--- karma.conf.js
+++ karma.conf.js
@@ -16,4 +16,81 @@ module.exports = function(config) {
     reporters: ['mocha'],
     browsers: ["ChromeHeadless"]
   });
+
+  if (process.env.CI) {
+    config.set({
+      customLaunchers: {
+        'SL_Chrome': {
+          base: 'SauceLabs',
+          browserName: 'chrome',
+          version: 'latest'
+        },
+        'SL_Chrome-1': {
+          base: 'SauceLabs',
+          browserName: 'chrome',
+          version: 'latest-1'
+        },
+        'SL_Firefox': {
+          base: 'SauceLabs',
+          browserName: 'firefox',
+          version: 'latest'
+        },
+        'SL_Firefox-1': {
+          base: 'SauceLabs',
+          browserName: 'firefox',
+          version: 'latest-1'
+        },
+        'SL_Safari-1': {
+          base: 'SauceLabs',
+          browserName: 'safari',
+          version: 'latest-1'
+        },
+        'SL_Safari': {
+          base: 'SauceLabs',
+          browserName: 'safari',
+          version: 'latest'
+        },
+        'SL_IE_9': {
+          base: 'SauceLabs',
+          browserName: 'internet explorer',
+          platform: 'Windows 2008',
+          version: '9'
+        },
+        'SL_IE_10': {
+          base: 'SauceLabs',
+          browserName: 'internet explorer',
+          platform: 'Windows 2012',
+          version: '10'
+        },
+        'SL_IE_11': {
+          base: 'SauceLabs',
+          browserName: 'internet explorer',
+          platform: 'Windows 8.1',
+          version: '11'
+        },
+        'SL_EDGE': {
+          base: 'SauceLabs',
+          browserName: 'microsoftedge',
+          platform: 'Windows 10',
+          version: 'latest'
+        },
+        'SL_EDGE-1': {
+          base: 'SauceLabs',
+          browserName: 'microsoftedge',
+          platform: 'Windows 10',
+          version: 'latest-1'
+        },
+        'SL_iOS': {
+          base: 'SauceLabs',
+          browserName: 'iphone',
+          version: 'latest'
+        },
+        'SL_iOS-1': {
+          base: 'SauceLabs',
+          browserName: 'iphone',
+          version: 'latest-1'
+        }
+      }
+    })
+  }
 };
