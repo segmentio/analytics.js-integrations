fe17a1a551697d87b35b1266da4a5f4d2d3c7066
Gurdas Nijor <nijor22@gmail.com>
2019-07-10 12:34:49 -0700

    Stub in tests

---
+++ integrations/visual-tagger/test/index.test.js
@@ -0,0 +1,42 @@
+'use strict';
+
+var Analytics = require('@segment/analytics.js-core').constructor;
+var integration = require('@segment/analytics.js-integration');
+var sandbox = require('@segment/clear-env');
+var tester = require('@segment/analytics.js-integration-tester');
+var Tracktor = require('../lib/');
+
+describe('Tracktor', function() {
+  var analytics;
+  var tracktor;
+  var options = {
+    workspaceId: 'a-workspace-id',
+    sourceId: 'a-source-id'
+  };
+
+  beforeEach(function() {
+    analytics = new Analytics();
+    tracktor = new Tracktor(options);
+    analytics.use(Tracktor);
+    analytics.use(tester);
+    analytics.add(tracktor);
+  });
+
+  afterEach(function(done) {
+    analytics.restore();
+    analytics.reset();
+    tracktor.reset();
+    sandbox();
+    done();
+  });
+
+  it('should have the right settings', function() {
+    analytics.compare(
+      Tracktor,
+      integration('Tracktor')
+        .global('Tracktor')
+        .option('sourceId', '')
+        .option('workspaceId', '')
+    );
+  });
+});
