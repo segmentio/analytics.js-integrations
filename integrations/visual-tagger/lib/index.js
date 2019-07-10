c2aeb7897ac089950273c2d6b95924d4a135885b
Gurdas Nijor <nijor22@gmail.com>
2019-07-10 12:26:51 -0700

    Thread through integration-specific options

--- integrations/visual-tagger/lib/index.js
+++ integrations/visual-tagger/lib/index.js
@@ -1,16 +1,18 @@
-'use strict';
-
 /**
  * Module dependencies.
  */
 // var TracktorLib = require('@segment/tracktor');
 var integration = require('@segment/analytics.js-integration');
+var TracktorLib = require('@segment/tracktor');
 
 /**
  * Expose `Tracktor` integration.
  */
 
-var Tracktor = (module.exports = integration('Tracktor').global('tracktor'));
+var Tracktor = (module.exports = integration('Tracktor')
+  .global('Tracktor')
+  .option('workspaceId', '')
+  .option('sourceId', ''));
 
 /**
  * Initialize.
@@ -18,7 +20,16 @@ var Tracktor = (module.exports = integration('Tracktor').global('tracktor'));
  * @api public
  */
 
-Tracktor.prototype.initialize = function() {};
+Tracktor.prototype.initialize = function() {
+  var tracktor = new TracktorLib(
+    this.options.workspaceId,
+    this.options.sourceId,
+    window.document
+  );
+  window.Tracktor = TracktorLib;
+
+  tracktor.start();
+};
 
 /**
  * Loaded?
@@ -28,5 +39,5 @@ Tracktor.prototype.initialize = function() {};
  */
 
 Tracktor.prototype.loaded = function() {
-  return true;
+  return window.Tracktor.instance.initialized;
 };
