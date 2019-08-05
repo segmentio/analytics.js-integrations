de7324819907320176efea0e74c2b185c902c82f
Aneesh Butani <aneesh@fullstory.com>
2019-08-05 13:52:33 -0400

    Don't load fs.js again

--- integrations/fullstory/lib/index.js
+++ integrations/fullstory/lib/index.js
@@ -37,11 +37,10 @@ FullStory.prototype.initialize = function() {
 
   /* eslint-disable */
   /* istanbul ignore next */
+  /* The snippet below differs slightly from the snippet available on fullstory.com because fs.js is already loaded above*/
   (function(m,n,e,t,l,o,g,y){
     if (e in m) {if(m.console && m.console.log) { m.console.log('FullStory namespace conflict. Please set window["_fs_namespace"].');} return;}
     g=m[e]=function(a,b,s){g.q?g.q.push([a,b,s]):g._api(a,b,s);};g.q=[];
-    o=n.createElement(t);o.async=1;o.crossOrigin='anonymous';o.src='https://'+_fs_host+'/s/fs.js';
-    y=n.getElementsByTagName(t)[0];y.parentNode.insertBefore(o,y);
     g.identify=function(i,v,s){g(l,{uid:i},s);if(v)g(l,v,s)};g.setUserVars=function(v,s){g(l,v,s)};g.event=function(i,v,s){g('event',{n:i,p:v},s)};
     g.shutdown=function(){g("rec",!1)};g.restart=function(){g("rec",!0)};
     g.log = function(a,b) { g("log", [a,b]) };
