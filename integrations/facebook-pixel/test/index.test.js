0e11da23050a0a667d227e715e3652c718df9981
Brie <brienne.mcnally@segment.com>
2019-09-19 16:57:25 -0700

    Add unit tests for new functionality

--- integrations/facebook-pixel/test/index.test.js
+++ integrations/facebook-pixel/test/index.test.js
@@ -2554,6 +2554,45 @@ describe('Facebook Pixel', function() {
 
       analytics.deepEqual(expected, actual);
     });
+
+    it('should add an external_id if defined in settings', function() {
+      facebookPixel.options.keyForExternalId = 'test_external_id';
+      analytics.identify('123', {
+        firstName: 'brie',
+        lastName: 'test',
+        test_external_id: '123456789',
+        address: null
+      });
+      var expected = {
+        fn: 'brie',
+        ln: 'test',
+        ge: 'm',
+        db: '19910113',
+        external_id: '123456789'
+      };
+      var actual = facebookPixel.formatTraits(analytics);
+
+      analytics.deepEqual(expected, actual);
+    });
+
+    it('should reject an external_id if NOT defined in settings', function() {
+      facebookPixel.options.keyForExternalId = '';
+      analytics.identify('123', {
+        firstName: 'brie',
+        lastName: 'test',
+        test_external_id: '123456789',
+        address: null
+      });
+      var expected = {
+        fn: 'brie',
+        ln: 'test',
+        ge: 'm',
+        db: '19910113'
+      };
+      var actual = facebookPixel.formatTraits(analytics);
+
+      analytics.deepEqual(expected, actual);
+    });
   });
 
   describe('#merge', function() {
