3053089bf5db79b0af0fd00c2d77a63a50aabcef
SegmentDestinationsBot <tools+github-destinations-bot@segment.com>
2019-10-17 15:03:41 -0700

    Add integration google-adwords-new
    This commit copies the content of the integration repo into
    the "integrations" folder.
    
    Original repo: https://github.com/segment-integrations/analytics.js-integration-google-adwords-new
    Readme: https://github.com/segment-integrations/analytics.js-integration-google-adwords-new/blob/master/README.md

---
+++ integrations/google-adwords-new/README.md
@@ -0,0 +1,8 @@
+[![CircleCI](https://circleci.com/gh/segment-integrations/analytics.js-integration-google-adwords-new.svg?style=svg)](https://circleci.com/gh/segment-integrations/analytics.js-integration-google-adwords-new)
+# analytics.js-integration-google-adwords-new
+
+Google AdWords New for Analytics.js integrations.
+
+## License
+
+Released under the [MIT license](LICENSE).
---
+++ integrations/google-adwords-new/lib/index.js
@@ -0,0 +1,178 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+
+var integration = require('@segment/analytics.js-integration');
+var each = require('@ndhoule/each');
+var find = require('obj-case');
+var reject = require('reject');
+var extend = require('extend');
+
+/**
+ * Expose `GoogleAdWordsNew` integration.
+ */
+
+var GoogleAdWordsNew = module.exports = integration('Google AdWords New')
+  .option('accountId', '')
+  .option('sendPageView', true)
+  .option('conversionLinker', true)
+  .option('clickConversions', [])
+  .option('pageLoadConversions', [])
+  .option('defaultPageConversion', '')
+  // The ID in this line (i.e. the gtag.js ID) does not determine which account(s) will receive data from the tag; rather, it is used to uniquely identify your global site tag. Which account(s) receive data from the tag is determined by calling the config command (and by using the send_to parameter on an event). For instance, if you use Google Analytics, you may already have the gtag.js global site tag installed on your site. In that case, the gtag.js ID may be that of the Google Analytics property where you first obtained the snippet.
+  .tag('<script src="https://www.googletagmanager.com/gtag/js?id={{ accountId }}">');
+
+/**
+ * Initialize.
+ *
+ * https://support.google.com/adwords/answer/6095821?hl=en&ref_topic=3165803
+ * @api public
+ */
+
+GoogleAdWordsNew.prototype.initialize = function() {
+  var self = this;
+  window.dataLayer = window.dataLayer || [];
+  window.gtag = function() {
+    window.dataLayer.push(arguments);
+  };
+
+  var config = {};
+  if (this.options.sendPageView === false) config.send_page_view = this.options.sendPageView;
+  if (this.options.conversionLinker === false) config.conversion_linker = this.options.conversionLinker; // not recommended to set this by GA docs — less accurate measurements
+
+  this.load(function() {
+    window.gtag('js', new Date());
+    window.gtag('config', self.options.accountId, config);
+    self.ready();
+  });
+};
+
+/**
+ * Loaded?
+ *
+ * @api public
+ * @return {boolean}
+ */
+
+GoogleAdWordsNew.prototype.loaded = function() {
+  return window.dataLayer.push !== Array.prototype.push;
+};
+
+/**
+ * Page
+ *
+ * https://support.google.com/adwords/answer/6095821?hl=en&ref_topic=3165803
+ * @api public
+ */
+
+GoogleAdWordsNew.prototype.page = function(page) {
+  var self = this;
+  // If there is no pageName, we support firing default Page Load conversion if provided in options
+  var configs = this.options;
+  var pageName = page.name();
+  // If you are naming your `.page()` calls, you should explicitly map each one rather than expecting a fallback to default page conversion
+  if (!pageName && configs.defaultPageConversion) return sendPageLoadConversion(configs.defaultPageConversion);
+
+  // A mapped event can either be a 'Page Load' or a 'Click' conversion in AdWords.
+  // Since the Page Load conversions are meant to just be dropped on a given page, we are mapping named page calls rather than `.track()`
+  var mappedConversion = matchConversion(this.options.pageLoadConversions, pageName);
+
+  if (mappedConversion.id) return sendPageLoadConversion(mappedConversion.id, mappedConversion.override);
+
+  function sendPageLoadConversion(id, override) {
+    var semanticMetadata = reject({
+      send_to: (override || configs.accountId) + '/' + id,
+      // the below are not spec'd props of page API, AdWords accepts can accept them for this type of conversions
+      value: page.options(self.name).value,
+      currency: page.options(self.name).currency,
+      transaction_id: find(page.options(self.name), 'order_id')
+    });
+    var metadata = extend(page.properties(), semanticMetadata);
+    // metadata shouldn't contain PII — warning by Google
+    return window.gtag('event', pageName || 'conversion', metadata);
+  }
+};
+
+/**
+ * Track
+ *
+ * https://support.google.com/adwords/answer/6331314?hl=en&ref_topic=3165803&co=ADWORDS.IsAWNCustomer%3Dtrue&oco=0
+ * https://developers.google.com/adwords-remarketing-tag/
+ * NOTE: If customers want to send conversions to multiple accounts, they should:
+ * "Alternatively, you should consider using cross-account conversion tracking which allows you to have a manager account (MCC) own
+ * the conversion actions and share them with one or more of its sub-accounts. In doing so, you only need to specify a single conversion
+ * identifier in the event snippet. Learn more about cross- account conversion tracking."
+ *
+ * But assuming pushback, we will allow overriding the default AdWords account ID per `.track()` if needed via UI settings
+ * NOTE: Conversion tracking and remarketing functionalities are now combined into a single tag so no need for feature flag for remarketing
+ *
+ * @api public
+ */
+
+GoogleAdWordsNew.prototype.track = function(track) {
+  var self = this;
+  // A mapped event can either be a 'Page Load' or a 'Click' conversion in AdWords.
+  // Depending on what you chose inside Adwords when creating the conversions, we should expect `properties.value` if that is what they want to send
+  // But for purchase events, we should map revenue/total
+  var eventName = track.event();
+  var mappedConversion = matchConversion(this.options.clickConversions, track.event());
+
+  if (mappedConversion.id) {
+    var properties = track.properties({ orderId: 'transaction_id' });
+    var metadata = extend(properties, { send_to: (mappedConversion.override || self.options.accountId) + '/' + mappedConversion.id });
+    // metadata shouldn't contain PII — warning by Google
+    return window.gtag('event', eventName, metadata);
+  }
+};
+
+/**
+ * Order Completed
+ *
+ * https://support.google.com/adwords/answer/6331314?hl=en&ref_topic=3165803&co=ADWORDS.IsAWNCustomer%3Dtrue&oco=0
+ * @api public
+ */
+
+GoogleAdWordsNew.prototype.orderCompleted = function(track) {
+  var self = this;
+  // A mapped event can either be a 'Page Load' or a 'Click' conversion in AdWords.
+  // Depending on what you chose inside Adwords when creating the conversions, we should expect `properties.value` if that is what they want to send
+  // But for purchase events, we should map revenue/total
+  var eventName = track.event();
+  var mappedConversion = matchConversion(this.options.clickConversions, track.event());
+
+  if (mappedConversion.id) {
+    var properties = track.properties({
+      orderId: 'transaction_id',
+      order_id: 'transaction_id',
+      revenue: 'value'
+    });
+    var metadata = extend(properties, { send_to: (mappedConversion.override || self.options.accountId) + '/' + mappedConversion.id });
+    // metadata shouldn't contain PII — warning by Google
+    return window.gtag('event', eventName, metadata);
+  }
+};
+
+/**
+ * Match Mapped AdWords conversions to your `.page()` or `.track()` calls
+ *
+ * @param {Array} mappedConversions
+ * @param {String} segmentEvent
+ */
+
+function matchConversion(mappedConversions, segmentEvent) {
+  var ret = {};
+  each(function(setting) {
+    var conversion = setting.value || setting;
+
+    // to prevent common casing mistakes in our UI
+    if (segmentEvent.toLowerCase() === conversion.event.toLowerCase()) {
+      ret.id = conversion.id;
+      // in case customer has multiple AdWords accounts
+      if (conversion.accountId) ret.override = conversion.accountId;
+    }
+  }, mappedConversions);
+
+  return ret;
+}
