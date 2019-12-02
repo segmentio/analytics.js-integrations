c47d08000a0b3faabeda7dea6414dda465508022
Vijayraj <vijayraj.nathe@weboniselab.com>
2019-12-02 21:10:15 +0530

    Referring external_id from ajs_user_id or ajs_anonymous_id if not present in traits

--- integrations/facebook-pixel/lib/index.js
+++ integrations/facebook-pixel/lib/index.js
@@ -626,6 +626,10 @@ FacebookPixel.prototype.formatTraits = function formatTraits(analytics) {
   if (this.options.keyForExternalId) {
     external_id = traits[this.options.keyForExternalId]; // eslint-disable-line
   }
+  if (!external_id && analytics) { // eslint-disable-line
+    var userData = analytics.user().options();
+    external_id = userData.ajs_user_id || userData.ajs_anonymous_id; // eslint-disable-line
+  }
   return reject({
     em: traits.email,
     fn: firstName,
