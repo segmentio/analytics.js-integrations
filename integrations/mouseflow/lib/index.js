b1e5e0e5e4b84062d154e61f3a2e66c6383ee61b
SegmentDestinationsBot <tools+github-destinations-bot@segment.com>
2019-12-10 14:23:18 -0800

    Add integration mouseflow
    This commit copies the content of the integration repo into
    the "integrations" folder.
    
    Original repo: https://github.com/segment-integrations/analytics.js-integration-mouseflow
    Readme: https://github.com/segment-integrations/analytics.js-integration-mouseflow/blob/master/README.md

---
+++ integrations/mouseflow/lib/index.js
@@ -0,0 +1,98 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+
+var each = require('component-each');
+var integration = require('@segment/analytics.js-integration');
+var push = require('global-queue')('_mfq');
+
+/**
+ * Expose `Mouseflow`.
+ */
+
+var Mouseflow = module.exports = integration('Mouseflow')
+  .assumesPageview()
+  .global('_mfq')
+  .global('mouseflow')
+  .global('mouseflowHtmlDelay')
+  .option('apiKey', '')
+  .option('mouseflowHtmlDelay', 0)
+  .tag('<script src="//cdn.mouseflow.com/projects/{{ apiKey }}.js">');
+
+/**
+ * Initalize.
+ *
+ * @api public
+ */
+
+Mouseflow.prototype.initialize = function() {
+  window.mouseflowHtmlDelay = this.options.mouseflowHtmlDelay;
+  this.load(this.ready);
+};
+
+/**
+ * Loaded?
+ *
+ * @api private
+ * @return {boolean}
+ */
+
+Mouseflow.prototype.loaded = function() {
+  return !!window.mouseflow;
+};
+
+/**
+ * Page.
+ *
+ * http://mouseflow.zendesk.com/entries/22528817-Single-page-websites
+ *
+ * @api public
+ * @param {Page} page
+ */
+
+Mouseflow.prototype.page = function(page) {
+  push('newPageView', page.path());
+};
+
+/**
+ * Identify.
+ *
+ * http://mouseflow.zendesk.com/entries/24643603-Custom-Variables-Tagging
+ *
+ * @api public
+ * @param {Identify} identify
+ */
+
+Mouseflow.prototype.identify = function(identify) {
+  set(identify.traits());
+};
+
+/**
+ * Track.
+ *
+ * http://mouseflow.zendesk.com/entries/24643603-Custom-Variables-Tagging
+ *
+ * @api public
+ * @param {Track} track
+ */
+
+Mouseflow.prototype.track = function(track) {
+  var props = track.properties();
+  props.event = track.event();
+  set(props);
+};
+
+/**
+ * Push each key and value in the given `obj` onto the queue.
+ *
+ * @api private
+ * @param {Object} obj
+ */
+
+function set(obj) {
+  each(obj, function(key, value) {
+    push('setVariable', key, value);
+  });
+}
