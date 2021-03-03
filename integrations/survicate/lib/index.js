b01fd0ebeed5158f8593f66ef89dd0f0a8dd3d6e
Netto Farah <nettofarah@gmail.com>
2021-03-03 14:13:07 -0800

    Update broken survicate dep

--- integrations/survicate/lib/index.js
+++ integrations/survicate/lib/index.js
@@ -8,7 +8,7 @@ var integration = require('@segment/analytics.js-integration');
 var when = require('do-when');
 var del = require('obj-case').del;
 var is = require('is');
-var snake = require('change-case').snake;
+var snake = require('change-case').snakeCase;
 var each = require('@ndhoule/each');
 
 
