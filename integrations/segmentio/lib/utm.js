c944c8cccfa6eb592f3819edf40915f934d34ddc
Julio <julio.farah@gmail.com>
2021-04-12 17:16:39 -0700

    remove polyfills and arrow func

--- integrations/segmentio/lib/utm.js
+++ integrations/segmentio/lib/utm.js
@@ -1,18 +1,11 @@
 function utm(query) {
-  // Polyfills
-  if (!String.prototype.startsWith) {
-    String.prototype.startsWith = function(search, rawPos) {
-      var pos = rawPos > 0 ? rawPos | 0 : 0
-      return this.substring(pos, pos + search.length) === search
-    }
-  }
-
-  if (query.startsWith('?')) {
+  if (query.lastIndexOf('?', 0) === 0) {
     query = query.substring(1)
   }
+
   query = query.replace(/\?/g, '&')
 
-  return query.split('&').reduce((acc, str) => {
+  return query.split('&').reduce(function(acc, str) {
     var k = str.split('=')[0]
     var v = str.split('=')[1]
 
