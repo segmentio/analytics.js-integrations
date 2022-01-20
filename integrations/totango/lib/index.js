be759cf4d64ee93afd3138a7df3e125682522332
shmulikgolan <33657710+shmulikgolan@users.noreply.github.com>
2022-01-20 20:05:29 +0200

    support Totango EU services (replace PR#650 per Pooyaj comment) (#652)
    This change needed to be supported with a new selection option, region, with the values "us" or "eu1"

--- integrations/totango/lib/index.js
+++ integrations/totango/lib/index.js
@@ -23,6 +23,7 @@ var Totango = (module.exports = integration('Totango')
   .option('serviceId', null)
   .option('trackNamedPages', true)
   .option('trackCategorizedPages', true)
+  .option('region', 'us')
   .tag('<script src="//tracker.totango.com/totango4.0.3.js">'));
 
 /**
@@ -47,6 +48,7 @@ Totango.prototype.initialize = function(page) {
   window.totango_options = {
     allow_empty_accounts: false,
     service_id: this.options.serviceId,
+    region: this.options.region,
     disable_heartbeat: this.options.disableHeartbeat,
     module: page.category()
   };
