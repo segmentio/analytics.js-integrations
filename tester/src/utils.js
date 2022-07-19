511ffad41c62678652364002984e569e4579930d
Pooya Jaferian <pooya.j@gmail.com>
2022-07-19 14:55:09 -0700

    Pj/tester (#688)
    * adding the build  command
    
    * adding the webpack config for the destinations
    
    * adding tester app
    
    * yarn.lock

---
+++ tester/src/utils.js
@@ -0,0 +1,39 @@
+import { AnalyticsBrowser } from '@segment/analytics-next';
+
+export async function getSettings(wk) {
+  let data = await fetch(`https://cdn.segment.com/v1/projects/${wk}/settings`);
+  data = await data.json();
+  return cleanSettings(data);
+}
+
+function cleanSettings(data) {
+  Object.keys(data.integrations).forEach(i => {
+    // setting the version of everything to latest
+    data.integrations[i].versionSettings.version = 'latest';
+    data.integrations[i].versionSettings.override = 'latest';
+  });
+
+  return data;
+}
+
+export async function loadAJS(writeKey, cdnSettings) {
+  Object.keys(cdnSettings.integrations).forEach(i => {
+    const name = i
+      .toLowerCase()
+      .replace('.', '')
+      .replace(/\s+/g, '-');
+
+    // we are not using common chunks for tester, so we can ignore the dependency resolution magic
+    window[`${name}Deps`] = [];
+    window[`${name}Loader`] = () => undefined;
+  });
+
+  const analytics = AnalyticsBrowser.load({
+    writeKey,
+    cdnURL: 'http://localhost:8080',
+    cdnSettings: { ...cdnSettings }
+  });
+
+  analytics.identify('hello world');
+  window.analytics = analytics;
+}
---
+++ webpack.config.tester.js
@@ -0,0 +1,74 @@
+const { strict } = require('assert');
+var path = require('path');
+var fs = require('fs-extra');
+var BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
+  .BundleAnalyzerPlugin;
+
+var files = fs.readdirSync('./integrations');
+
+var entries = {};
+
+files.forEach(function(file) {
+  var filePath = './integrations/' + file + '/lib/index.js';
+  if (!fs.existsSync(filePath)) {
+    filePath = './integrations/' + file + '/lib/index.ts';
+    if (!fs.existsSync(filePath)) {
+      return;
+    }
+  }
+  entries[file] = filePath;
+});
+
+var plugins = [];
+
+module.exports = {
+  entry: entries,
+  mode: process.env.NODE_ENV || 'development',
+  devtool: 'source-map',
+  output: {
+    filename: '[name]/latest/[name].dynamic.js.gz',
+    chunkFilename: 'vendor/[name].[contenthash].js.gz',
+    path: path.resolve(__dirname, 'tester/dist/next-integrations/integrations'),
+    library: '[name]Integration',
+    libraryTarget: 'window'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              [
+                '@babel/preset-env',
+                {
+                  targets: {
+                    ie: '11'
+                  }
+                }
+              ]
+            ]
+          }
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+    alias: {
+      domify: '/node_modules/domify/index.js',
+      '@segment/analytics.js-integration':
+        '/node_modules/@segment/analytics.js-integration'
+    }
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'build')
+  },
+  optimization: {
+    moduleIds: 'hashed',
+    minimize: false
+  },
+  plugins: plugins
+};
