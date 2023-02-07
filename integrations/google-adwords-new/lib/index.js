06e13b1ded830673d5b112f3c00848ea6626aae8
Robert Hall <rhall@twilio.com>
2023-02-07 16:13:17 -0600

    nit: add floodlightAccountId as an option to the initializer

--- integrations/google-adwords-new/lib/index.js
+++ integrations/google-adwords-new/lib/index.js
@@ -22,6 +22,7 @@ var GoogleAdWordsNew = (module.exports = integration('Google AdWords New')
   .option('pageLoadConversions', [])
   .option('defaultPageConversion', '')
   .option('disableAdPersonalization', false)
+  .option('floodlightAccountId', '')
   // The ID in this line (i.e. the gtag.js ID) does not determine which account(s) will receive data from the tag; rather, it is used to uniquely identify your global site tag. Which account(s) receive data from the tag is determined by calling the config command (and by using the send_to parameter on an event). For instance, if you use Google Analytics, you may already have the gtag.js global site tag installed on your site. In that case, the gtag.js ID may be that of the Google Analytics property where you first obtained the snippet.
   .tag(
     '<script src="https://www.googletagmanager.com/gtag/js?id={{ accountId }}">'
