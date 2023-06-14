23883763e765692c05631e8ee924fffc75702c96
Francisco Garcia <paco.g@walkme.com>
2023-06-14 17:56:30 +0200

    update literal to friendly name

--- integrations/walkme/lib/index.js
+++ integrations/walkme/lib/index.js
@@ -19,7 +19,7 @@ var WalkMe = module.exports = integration('WalkMe')
   .option('trackWalkMeEvents', false)
   .option('loadWalkMeInIframe', false)
   .option('integrityHash', '')
-  .option('bucketName', 'users')
+  .option('customDirecotry', 'users')
   .tag('<script async="true" src="{{ url }}" crossorigin="" integrity="{{ hash }}">')
 
 /**
@@ -68,7 +68,7 @@ WalkMe.prototype.initialize = function() {
     sriSuffix = 'private_';
   }
 
-  var bucket = (this.options.bucketName) ? this.options.bucketName : 'users';
+  var bucket = (this.options.customDirecotry) ? this.options.customDirecotry : 'users';
   var url = `https://cdn.walkme.com/${bucket}/${walkMeSystemId}/${env}/walkme_${sriSuffix}${walkMeSystemId}_https.js`;
 
   this.load({
